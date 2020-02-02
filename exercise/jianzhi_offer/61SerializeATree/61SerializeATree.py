cdabbee3f934a1491ff2ea439e4301b50b616b04
贲雅芳 <yafang1826@163.com>
2020-02-02 22:25:46 +0800

    :fire: python版本

---
+++ exercise/jianzhi_offer/61SerializeATree/61SerializeATree.py
@@ -0,0 +1,70 @@
+class TreeNode:
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+class SerializeATree:
+
+    def serializeCallback(self, root, arr):
+        if root == None:
+            arr.append("null")
+        else:
+            arr.append(root.val)
+            self.serializeCallback(root.left, arr)
+            self.serializeCallback(root.right, arr)
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        arr = []
+        self.serializeCallback(root, arr)
+        return ",".join(str(i) for i in arr)
+        
+    def deserializeCallback(self, arr):
+        if len(arr) == 0 or len(arr) == 1 and arr[0] == "null":
+            return None
+        value = arr.pop(0)
+        node = None
+        if value != "null":
+            print(value)
+            node = TreeNode(value)
+            node.left = self.deserializeCallback(arr)
+            node.right = self.deserializeCallback(arr)
+        return node
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        arr = data.split(",")
+        return self.deserializeCallback(arr)
+
+
+if __name__ == '__main__':
+    root = TreeNode(1)
+    node2 = TreeNode(2)
+    node3 = TreeNode(3)
+    node4 = TreeNode(4)
+    node5 = TreeNode(5)
+    root.left = node2
+    root.right = node3
+    node3.left = node4
+    node3.right = node5
+    serial = SerializeATree()
+    str = serial.serialize(root)
+    # print('ff')  
+    print(str)
+    node = serial.deserialize(str)
+
+    print(node)
+
+
+    
+
---
+++ exercise/jianzhi_offer/61SerializeATree/61SerializeATree1.js
@@ -0,0 +1,58 @@
+const {TreeNode} = require('../TreeNode');
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
+    let res = '';
+    function dfs(node) {
+        if (!node) {
+            res += '#,';
+            return;
+        }
+        res += node.val + ',';
+        dfs(node.left);
+        dfs(node.right);
+    }
+    dfs(root);
+    return res;
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
+    let index = 0;
+    function dfs() {
+        let start = index;
+        while (data[index] !== ',') index++;
+        const str = data.slice(start, index);
+        index++;
+        
+        if (str === '#') return null;
+        
+        const root = new TreeNode(Number(str));
+        root.left = dfs();
+        root.right = dfs();
+        return root;
+    }
+    return dfs();
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
